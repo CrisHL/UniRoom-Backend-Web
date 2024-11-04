19735c32b7c09604a2ab5456ad1e9708503d6a19
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-11-04 12:33:49 -0600

    UR: Se modifico la logica de los jwt y tambien se envia correo indicandole al usuario su nuevo inicio de sesio v5

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -136,69 +136,32 @@ export const sendNewLogin = async (name: string, email: string) => {
     to: email,
     subject: "Detectamos un inicio de sesión reciente en tu cuenta 👀",
     html: `
-      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-          <html dir="ltr" lang="en">
-
-  <head>
-    <link rel="preload" as="image" href="https://i.imgur.com/uKIVq1K.png" />
-    <link rel="preload" as="image" href="https://i.imgur.com/qzOLE9P.png" />
-    <link rel="preload" as="image" href="https://react-email-demo-3kjjfblod-resend.vercel.app/static/yelp-footer.png" />
-    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-    <meta name="x-apple-disable-message-reformatting" /><!--$-->
-  </head>
-
-  <body style="background-color:#fff;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Oxygen-Sans,Ubuntu,Cantarell,&quot;Helvetica Neue&quot;,sans-serif">
-    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="max-width:37.5em">
-      <tbody>
-        <tr style="width:100%">
-          <td>
-            <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="padding:30px 20px">
-              <tbody>
-                <tr>
-                  <td><img src="https://i.imgur.com/QePbXJh.png" style="display:block;outline:none;border:none;text-decoration:none" /></td>
-                </tr>
-              </tbody>
-            </table>
-            <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="border:1px solid rgb(0,0,0, 0.1);border-radius:3px;overflow:hidden">
-              <tbody>
-                <tr>
-                  <td>
-                    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
-                      <tbody style="width:100%">
-                        <tr style="width:100%"><img src="https://i.imgur.com/kBYoqdI.png" style="display:block;outline:none;border:none;text-decoration:none;max-width:100%" width="620" /></tr>
-                      </tbody>
-                    </table>
-                    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="padding:20px;padding-bottom:0">
-                      <tbody style="width:100%">
-                        <tr style="width:100%">
-                          <td data-id="__react-email-column">
-                            <h1 style="font-size:32px;font-weight:bold;text-align:center">Hola <!-- -->${name}<!-- -->,</h1>
-                            <h2 style="font-size:26px;font-weight:bold;text-align:center">Hemos detectado un nuevo inicio de sesión en tu cuenta de UniRoom 🚨</h2>
-                            <p style="font-size:16px;line-height:24px;margin:16px 0">Si no fuiste tú, te recomendamos cambiar tu contraseña de inmediato por seguridad.</p>
-                            <p style="font-size:16px;line-height:24px;margin:16px 0">Puedes hacerlo desde la página web: <a href="https://uniroom.app" style="color:#007bff;text-decoration:none">uniroom.app</a></p>
-                            <p style="font-size:16px;line-height:24px;margin:16px 0;margin-top:-5px">Saludos, equipo de UniRoom</p>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="padding:45px 0 0 0">
-              <tbody>
-                <tr>
-                  <td><img src="https://react-email-demo-3kjjfblod-resend.vercel.app/static/yelp-footer.png" style="display:block;outline:none;border:none;text-decoration:none;max-width:100%" width="620" /></td>
-                </tr>
-              </tbody>
-            </table>
-            <p style="font-size:12px;line-height:24px;margin:16px 0;text-align:center;color:rgb(0,0,0, 0.7)">© 2024 | UniRoom, Tula de Allende, Hidalgo, 42836, México | uniroom.app</p>
-          </td>
-        </tr>
-      </tbody>
-    </table><!--/$-->
-  </body>
-            </html>
-    `
+      <!DOCTYPE html>
+      <html lang="es">
+        <head>
+          <meta charset="UTF-8">
+        </head>
+        <body>
+          <table align="center" style="border-width:1px; border-style:solid; border-color:#eaeaea; border-radius:5px; margin-top:40px; max-width:465px; padding:20px;">
+            <tbody>
+              <tr>
+                <td>
+                  <img src="https://i.imgur.com/ruySKLQ.png" alt="Logo" height="130" style="display:block;margin:0 auto;">
+                  <h1 style="text-align:center; font-size:24px;">¡Hola, ${name}!<br></br> Hemos detectado un nuevo inicio de sesión 🚪🚶</h1>
+                  <p style="font-size:14px;">Si no has sido tú, te recomendamos cambiar la contraseña usando el siguiente botón:</p>
+                    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="text-align:center;margin-top:32px;margin-bottom:32px">
+                      <tbody>
+                       <tr>
+                        <td><a href="https://uniroom.app" style="background-color:rgb(0,0,0);border-radius:0.25rem;color:rgb(255,255,255);font-size:12px;font-weight:600;text-decoration-line:none;text-align:center;padding-left:1.25rem;padding-right:1.25rem;padding-top:0.75rem;padding-bottom:0.75rem;line-height:100%;text-decoration:none;display:inline-block;max-width:100%;mso-padding-alt:0px;padding:12px 20px 12px 20px" target="_blank"><span><!--[if mso]><i style="mso-font-width:500%;mso-text-raise:18" hidden>&#8202;&#8202;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9px">Recuperar contraseña</span><span><!--[if mso]><i style="mso-font-width:500%" hidden>&#8202;&#8202;&#8203;</i><![endif]--></span></a></td>
+                      </tr>
+                     </tbody>
+                  </table>
+                  <p style="font-size:14px;">Si has sido tú, ignora este mensaje y continúa buscando tu habitación ideal 😉🎓</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </body>
+      </html>`
   });
 };
